9c0c1b2ee991e311c0d8864e74ac603df764dc20
IvaSerge <ivaserge@ukr.net>
2020-02-01 11:30:26 +0100

    first commit

---
+++ MoveToCenter.py
@@ -0,0 +1,26 @@
+import clr
+clr.AddReference('RevitAPI')
+import Autodesk
+from Autodesk.Revit.DB import *
+
+import sys
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
+import System
+from System import Array
+from System.Collections.Generic import *
+
+import itertools
+import math
+
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+doc = DocumentManager.Instance.CurrentDBDocument
+uidoc=DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+
+
+
+OUT = None
