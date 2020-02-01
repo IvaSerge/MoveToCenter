c2c3cc30efc7bcf330a378bc7926719a50c9855a
IvaSerge <ivaserge@ukr.net>
2020-02-01 13:55:13 +0100

    Object selection

--- MoveToCenter.py
+++ MoveToCenter.py
@@ -21,6 +21,33 @@ from RevitServices.Transactions import TransactionManager
 doc = DocumentManager.Instance.CurrentDBDocument
 uidoc=DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
 
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.UI import *
+
+uiapp = DocumentManager.Instance.CurrentUIApplication
+uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+app = uiapp.Application
+
+reLoad = IN[0]
+selob = uidoc.Selection.PickObjects(
+            Autodesk.Revit.UI.Selection.ObjectType.Element,
+            "Selection of two elements")
+
+if len(selob) != 2:
+    raise ValueError("Only 2 elements need to be selected")
+
+lastob = uidoc.Selection.PickObject(
+            Autodesk.Revit.UI.Selection.ObjectType.Element,
+            "Selection of two elements")
+
+refList = list()
+map(lambda x: refList.append(x), selob)
+refList.append(lastob)
+
+obList = list()
+map(lambda x: obList.append(doc.GetElement(x.ElementId)), refList)
+
+#new point coordinates calculation
 
 
-OUT = None
+OUT = obList
